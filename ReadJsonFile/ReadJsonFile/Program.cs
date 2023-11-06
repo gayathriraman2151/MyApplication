d58af7a1c0b1396116a7d94103c07bf0be2d51b1
gayathriraman2151 <149969181+gayathriraman2151@users.noreply.github.com>
2023-11-05 20:12:07 -0800

    First Commit for ReadJsonFile

---
+++ ReadJsonFile/ReadJsonFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ReadJsonFile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString =
+@"{
+   ""Name"": ""Milk"",
+    ""Description"": ""Dairy Product"",
+    ""Type"": ""Grocery"",
+    ""InventoryItem"":{
+        ""Price"": 2.49,
+        ""Quantity"": 2
+    }
+    
+}
+";
+            ReadJsonObject.MainObject data = JsonConvert.DeserializeObject<ReadJsonObject.MainObject>(jsonString);
+
+            Console.WriteLine("Read Json Grocery Items ");
+            Console.WriteLine();
+            Console.WriteLine("Name: " + data.Name);
+            Console.WriteLine("Description: " + data.Description);
+            Console.WriteLine("Type: " + data.Type);
+            Console.WriteLine();
+            Console.WriteLine("Inventory");
+            Console.WriteLine("Price: " + data.InventoryItem.Price);
+            Console.WriteLine("Quantity: " + data.InventoryItem.Quantity);
+
+        }
+            
+    }
+}
---
+++ ReadJsonFile/ReadJsonFile/ReadJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJsonFile
+{
+    public class ReadJsonObject
+    {
+        public class MainObject
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Type { get; set; }
+            
+            public Inventory InventoryItem {  get; set; } 
+
+            public class Inventory
+            {
+                public float Price { get; set; }
+                public int Quantity { get; set; }
+
+            }
+        }
+    }
+}

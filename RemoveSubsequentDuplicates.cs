42e37541ab1e187e8ba8a2b6c6da189b9cbf83db
gayathriraman2151 <149969181+gayathriraman2151@users.noreply.github.com>
2023-11-05 20:47:32 -0800

    Remove subsequent duplicate items in the array

---
+++ RemoveSubsequentDuplicates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class RemoveSubsequentDuplicates
+{
+    //Remove subsequent duplicate items in the array.
+
+    public static void Main(String[] args)
+    {
+        //Creating a hash set
+        HashSet<int> hsRemoveSubsequentDupes = new HashSet<int>();
+
+        int[] arrayList = { 1, 2, 3, 3, 4, 5, 5, 5 };
+        int arrayLength = arrayList.Length;
+        
+        //Looping through Array
+        for (int i = 0; i < arrayLength; i++)
+        {
+
+            if (!hsRemoveSubsequentDupes.Contains(arr[i]))
+            {
+                hsRemoveSubsequentDupes.Add(arr[i]);
+            }
+
+        }
+        
+        // Display Elements
+        foreach (var element in hsRemoveSubsequentDupes)
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+}
+
+/* 
+Output:
+
+1
+2
+3
+4
+5
+ */
+*/
+ */
+ */

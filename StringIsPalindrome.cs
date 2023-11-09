91b37f0e03b6edb62309f7ed36847e3e39d8793f
gayathriraman2151 <149969181+gayathriraman2151@users.noreply.github.com>
2023-11-08 17:22:06 -0800

    String Is Palindrome

---
+++ StringIsPalindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StringIsPalindrome
+{
+
+    private static string ReverseAString(string s)
+    {
+        char[] strPalindrome = s.ToCharArray();
+        Array.Reverse(strPalindrome);
+        return new String(strPalindrome);
+
+    }
+    public static void Main(String[] args)
+    {
+        string strPalindrome = "";
+        if (strPalindrome == "")
+        {
+            Console.WriteLine("String cannot be blank");
+            return;
+        }
+        string reverseString = ReverseAString(strPalindrome);
+        if (strPalindrome == reverseString)
+        {
+            Console.WriteLine("String is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("String is not a Palindrome");
+        }
+
+    }
+
+}
+
